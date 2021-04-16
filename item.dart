6c51bb69faa8c359a871f4d3d219fbbeaf87943b
cyonglin18 <54050964+cyonglin18@users.noreply.github.com>
2021-04-16 22:17:55 +0800

    Add files via upload

---
+++ item.dart
@@ -0,0 +1,13 @@
+class Item {
+  String storeName;
+  String itemName;
+  String item;
+  String itemType;
+  DateTime purchaseDate;
+  DateTime expiryDate;
+  String status;
+  int quantity;
+
+  Item(this.storeName, this.itemName, this.item, this.itemType,
+      this.purchaseDate, this.expiryDate, this.status, this.quantity);
+}
